154b26f21ea63da4aa3edf14c433d666d014b07b
carlincb <carlin.colleen@gmail.com>
2021-10-03 15:53:56 -0600

    Fixed for loop error with renderForecastWeather function.

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,10 +21,9 @@ searchBtn.addEventListener("click", function(event){
         .then((response)=>response.json())
         .then(function(data){
             console.log(data);
-            for(i=0; i<=5; i++){
+            for(let i=0; i< data.daily.length; i++){
                 renderForecastWeather(data.daily[i]);
             }
-            renderForecastWeather(data);
         })
     })
 });
@@ -49,68 +48,16 @@ function renderCurrentWeather(weatherData){
 };
 
 function renderForecastWeather(weatherData){
-    var tempForecastA = document.createElement("p");
-    var windForecastA = document.createElement("p");
-    var humidityForecastA = document.createElement("p");
+    var tempForecast = document.createElement("p");
+    var windForecast = document.createElement("p");
+    var humidityForecast = document.createElement("p");
 
-    tempForecastA.innerText = `${weatherData.daily[0].temp.day}`;
-    currentWeather.append(tempForecastA);
+    tempForecast.innerText = `${weatherData.temp.day}`;
+    currentWeather.append(tempForecast);
 
-    windForecastA.innerText = `${weatherData.daily[0].wind_speed}`;
-    currentWeather.append(windForecastA);
+    windForecast.innerText = `${weatherData.wind_speed}`;
+    currentWeather.append(windForecast);
 
-    humidityForecastA.innerText = `${weatherData.daily[0].humidity}`;
-    currentWeather.append(humidityForecastA);
-// -------------------------------------------------------------------
-    var tempForecastB = document.createElement("p");
-    var windForecastB = document.createElement("p");
-    var humidityForecastB = document.createElement("p");
-
-    tempForecastB.innerText = `${weatherData.daily[1].temp.day}`;
-    currentWeather.append(tempForecastB);
-
-    windForecastB.innerText = `${weatherData.daily[1].wind_speed}`;
-    currentWeather.append(windForecastB);
-
-    humidityForecastB.innerText = `${weatherData.daily[1].humidity}`;
-    currentWeather.append(humidityForecastB);
-// -------------------------------------------------------------------
-    var tempForecastC = document.createElement("p");
-    var windForecastC = document.createElement("p");
-    var humidityForecastC = document.createElement("p");
-
-    tempForecastC.innerText = `${weatherData.daily[2].temp.day}`;
-    currentWeather.append(tempForecastC);
-
-    windForecastC.innerText = `${weatherData.daily[2].wind_speed}`;
-    currentWeather.append(windForecastC);
-
-    humidityForecastC.innerText = `${weatherData.daily[2].humidity}`;
-    currentWeather.append(humidityForecastC);
-// -------------------------------------------------------------------
-    var tempForecastD = document.createElement("p");
-    var windForecastD = document.createElement("p");
-    var humidityForecastD = document.createElement("p");
-
-    tempForecastD.innerText = `${weatherData.daily[3].temp.day}`;
-    currentWeather.append(tempForecastD);
-
-    windForecastD.innerText = `${weatherData.daily[3].wind_speed}`;
-    currentWeather.append(windForecastD);
-
-    humidityForecastD.innerText = `${weatherData.daily[3].humidity}`;
-    currentWeather.append(humidityForecastD);
-// -------------------------------------------------------------------
-    var tempForecastE = document.createElement("p");
-    var windForecastE = document.createElement("p");
-    var humidityForecastE = document.createElement("p");
-
-    tempForecastE.innerText = `${weatherData.daily[4].temp.day}`;
-    currentWeather.append(tempForecastE);
-
-    windForecastE.innerText = `${weatherData.daily[4].wind_speed}`;
-    currentWeather.append(windForecastE);
-
-    humidityForecastE.innerText = `${weatherData.daily[4].humidity}`;
-    currentWeather.append(humidityForecastE);
+    humidityForecast.innerText = `${weatherData.humidity}`;
+    currentWeather.append(humidityForecast);
 };
