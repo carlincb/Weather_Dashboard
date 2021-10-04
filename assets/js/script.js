cbe6e9e7d2a9161408d1762c828a042f85578adb
carlincb <carlin.colleen@gmail.com>
2021-10-03 21:18:18 -0600

    Added uvi.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 var APIKey = "9bd5c8436dd36e9efcab1ac13d881673";
 
+var currentCity;
 var currentWeather = document.getElementById("currentWeather");
 
 var searchText = document.getElementById("searchText");
@@ -15,12 +16,13 @@ searchBtn.addEventListener("click", function(event){
     .then((response)=>response.json())
     .then(function(data){
         console.log(data);
-        renderCurrentWeather(data);
+        currentCity = data.name
         var forecastURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${data.coord.lat}&lon=${data.coord.lon}&appid=${APIKey}&units=imperial`;
         fetch(forecastURL)
         .then((response)=>response.json())
         .then(function(data){
             console.log(data);
+            renderCurrentWeather(data);
             for(let i=0; i< data.daily.length; i++){
                 renderForecastWeather(data.daily[i]);
             }
@@ -33,18 +35,21 @@ function renderCurrentWeather(weatherData){
     var temp = document.createElement("p");
     var wind = document.createElement("p");
     var humidity = document.createElement("p");
+    var uvi = document.createElement("p");
 
-    cityNameHeader.innerText = `${weatherData.name}`;
+    cityNameHeader.innerText = `${currentCity}`;
     currentWeather.append(cityNameHeader);
 
-    temp.innerText = `${weatherData.main.temp}`;
+    temp.innerText = `${weatherData.current.temp}`;
     currentWeather.append(temp);
 
-    wind.innerText = `${weatherData.wind.speed}`;
+    wind.innerText = `${weatherData.current.wind_speed}`;
     currentWeather.append(wind);
 
-    humidity.innerText = `${weatherData.main.humidity}`;
+    humidity.innerText = `${weatherData.current.humidity}`;
     currentWeather.append(humidity);
+
+    uvi.innerText = `${weatherData.current.uvi}`;
 };
 
 function renderForecastWeather(weatherData){
